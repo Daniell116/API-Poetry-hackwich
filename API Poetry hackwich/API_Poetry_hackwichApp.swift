79bbe272c372d63156ddde547d6fec6d10d1b934
daniell escareno <daniellescareno7@gmail.com>
2026-02-27 08:26:10 -0800

    Initial Commit

---
+++ API Poetry hackwich/API_Poetry_hackwichApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_Poetry_hackwichApp.swift
+//  API Poetry hackwich
+//
+//  Created by daniell Escareno on 2/27/26.
+//
+
+import SwiftUI
+
+@main
+struct API_Poetry_hackwichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ API Poetry hackwich/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  API Poetry hackwich
+//
+//  Created by daniell Escareno on 2/27/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
